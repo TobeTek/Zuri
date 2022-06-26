a63c9f5c7408af7969611b48c6cb536471bae197
Emmanuel Katchy <82294671+TobeTek@users.noreply.github.com>
2022-06-26 23:00:10 +0100

    Create urls.py

---
+++ starter-files/Working With APIs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+app_name="link"
+
+urlpatterns = [
+    path("create/", views.PostCreateApi.as_view(), name="api_create"),
+    path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
+    path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
+    path("", views.PostListApi.as_view(), name="api_list"),
+]
